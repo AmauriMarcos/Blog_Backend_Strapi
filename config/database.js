973aab0a0a3a8c69ccd76b7769d8028258b64af0
AmauriMarcos <amaurisantos.m@gmail.com>
2020-07-21 13:33:38 +0200

    first commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.comnn.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'blog-backend'),
+        username: env('DATABASE_USERNAME', 'amaurisantos'),
+        password: env('DATABASE_PASSWORD', 'meuCPF37060257870'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '93cb28f8-8f67-4cab-b08b-8d763aab733e'
+};
