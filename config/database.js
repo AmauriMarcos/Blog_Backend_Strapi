268ff078610ca22a0e2f5bc370ac4c53160bde4f
AmauriMarcos <amaurisantos.m@gmail.com>
2020-07-21 13:58:24 +0200

    Update database config

--- config/database.js
+++ config/database.js
@@ -4,12 +4,14 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.comnn.mongodb.net'),
+        /* host: env('DATABASE_HOST', 'cluster0.comnn.mongodb.net'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'blog-backend'),
         username: env('DATABASE_USERNAME', 'amaurisantos'),
-        password: env('DATABASE_PASSWORD', 'meuCPF37060257870'),
+        password: env('DATABASE_PASSWORD', 'meuCPF37060257870'), */
+        "uri": "${process.env.DATABASE_URI}",
+        "database": "${process.env.DATABASE_NAME}"
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
