33ae2d0a0410b4ebbae7ba23114fe6cbdf09b90d
AmauriMarcos <amaurisantos.m@gmail.com>
2020-07-21 15:53:50 +0200

    another change

--- config/database.js
+++ config/database.js
@@ -4,13 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        /* "host": "${process.env.DATABASE_HOST}", */
-    /*     "srv":  env.bool('DATABASE_SRV', true), */
-       /*  "port": "${process.env.DATABASE_PORT}",
-        "database": "${process.env.DATABASE_NAME}",
-        "username": "${process.env.DATABASE_USERNAME}",
-        "password": "${process.env.DATABASE_PASSWORD}",
-        "uri": "${process.env.DATABASE_URI}" */
+ 
         host: `${process.env.DATABASE_HOST}`,
         srv: `${process.env.DATABASE_SRV}`,
         port: `${process.env.DATABASE_PORT}`,
@@ -18,11 +12,11 @@ module.exports = ({ env }) => ({
         username: `${process.env.DATABASE_USERNAME}`,
         password: `${process.env.DATABASE_PASSWORD}`,
         uri: `${process.env.DATABASE_URI}`
-        /* uri: "${process.env.DATABASE_URI}",
-        database: "${process.env.DATABASE_NAME}" */
+        /* uri: `${process.env.DATABASE_URI}`,
+        database: `${process.env.DATABASE_NAME}` */
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        authenticationDatabase: `${process.env.DATABASE_AUTHENTICATION_DATABASE}`,
         /* ssl: env.bool('DATABASE_SSL', true), */
         ssl: true
       },
